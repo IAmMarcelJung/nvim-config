bfefccd0dbfee15a909e7c38ad184dd57e1a9dc7
IAmMarcelJung <MarcelJung97@web.de>
2025-02-14 12:36:50 +0100

    fix: set all .v files to verilog hardcoded, since they sometimes get recognized as .coq files

--- init.lua
+++ init.lua
@@ -151,6 +151,13 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Set all .v files hardcoded as verilog files since neovim sometimes recognizes
+-- them as .coq files
+vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
+  pattern = '*.v',
+  command = 'set filetype=verilog',
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
